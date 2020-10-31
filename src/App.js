08cb1e1c286c85650e3a2ff9a619c166839cd80d
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-31 14:25:42 -0500

    comparation between a functional component and a class component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {};
-  }
+/* The functional component using the method useState provided by react allow us to reduce the amount 
+of code used on a class component method */
 
+function App() {
+  // it returns an array [null, f()]
+  // Array destructuring (the first passed to useState is given to the answer constant)
+  const [answer] = useState("No");
 
-  render() {
-    return (
-        <div>
-          Hello
-        </div>
-    )
-  }
-
+  return (
+      <div>
+        <h1>Is state important to know? : {answer}</h1>
+      </div>
+  );
 }
+
+// class App extends React.Component {
+
+//   constructor() {
+//     super();
+//     this.state = {
+//       answer: "Yes"
+//     };
+//   }
+
+
+//   render() {
+//     return (
+//         <div>
+//           <h1>Is state important to know? : {this.state.answer}</h1>
+//         </div>
+//     )
+//   }
+
+// }
 
 export default App;
