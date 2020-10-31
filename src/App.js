8eaaa28c5f2eddd2cf1f24474aa658757b524236
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-31 14:39:29 -0500

    added a externalized function, on the function component, in order to improve the readability of the code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  
+  function increment() {
+    /* 
+    usage of the function provided by the React.useState method
+    usage of the prev{State} and set a new value
+    */
+    setCount(prevCount => prevCount +1);
+  }
+  
   return (
       <div>
         <h1>{count}</h1>
         {/* Manipulation the State 
           anon function needed on the event onClick
-          usage of the function provided by the React.useState method
-          usage of the prev{State} and set a new value
+          calling an increment function (a function inside of our functional component this looks more organized)
         */}
-        <button onClick={() => {setCount(prevCount=> prevCount+1)}}>Change!</button>
+        <button onClick={increment}>Change!</button>
       </div>
   );
 }
