9cb24215f4946572522866b4c1aa2e455e17214d
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-31 19:22:09 -0500

    way to use useEffect to simulate the componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import randomcolor from 'randomcolor'
 
 function App() {
   const [count, setCount] = useState(0);
-  // instead of use an object to store all the values on the state 
-  // we can create an independent state for that specific value 
-  // with the method useState.
   const [alive, setAlive] = useState("Yes");
+  const [color, setColor] = useState("")
   
 
   function performOperation(operation) {
@@ -31,9 +30,56 @@ function App() {
     setAlive(prevAlive => prevAlive === "Yes" ? "No" : "Yes")
   }
 
+  /*
+  // 1.
+  // every single time that the component is rendered this function is called
+  // the setColor method is changing the state and that cause a render that cause an infinite loop
+  useEffect(() => {
+      // 1.
+      // setColor(randomcolor())
+  })*/
+
+  /*
+    lifecycle: ComponentDidUpdate
+
+  On this time we are specifying when the use effect callback method is going to be executed
+    as you can see in the array we add the property count, if the property count have a change 
+    then the UseEffect calback method is going to be executed... 
+  */
+  useEffect(() => {
+    setColor(randomcolor())
+  }, [count])
+
+  /* 
+    lifecycle: ComponentDidMount
+
+    If you want to simulate the componentDidMount Lifecycle woy cant do it in this way 
+    the callback code will be executed just every time that the component is mounted
+  */
+  useEffect(() => {
+    setColor(randomcolor())
+  }, [])
+
+  /*
+    lifecycle: ComponentWillUnmount
+    This time we are returning an anon function that will be stored by React and will be executed
+    when the component will be unmounted
+
+    the componentWillUnmount is used for clean some logic that can be running after go out the component.
+  */
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+        setCount(prevCount => prevCount + 1)
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+    
+  }, [])
+
+
   return (
       <div>
-        <h1>{count}</h1>
+        <h1 style={{color: color}}>{count}</h1>
         <h1>{alive}</h1>
         <button onClick={() => performOperation('+')}>Increment!</button>
         <button onClick={() => performOperation('-')}>Decrement!</button>
